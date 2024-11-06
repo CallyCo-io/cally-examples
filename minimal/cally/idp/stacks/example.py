335fac40edb5b4d9f15615442a54a0e30333326c
Leon Wright <techman83@gmail.com>
2024-11-06 17:09:26 +0800

    test: Add Output Tests
    This adds output tests to the example stack

--- minimal/cally/idp/stacks/example.py
+++ minimal/cally/idp/stacks/example.py
@@ -1,5 +1,10 @@
 from cally.cdk import CallyStack
+from cally.cli.config.config_types import CallyStackService
 
 
 class ExampleStack(CallyStack):
-    pass
+
+    def __init__(self, service: CallyStackService) -> None:
+        super().__init__(service)
+        self.add_output('foo', self.service.get_stack_var('foo', 'foo'))
+        self.add_output('bar', self.service.get_stack_var('bar', 'foo'))
